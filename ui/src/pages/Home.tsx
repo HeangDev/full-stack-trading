1ab5789eec0d9f6d345dccb4b1d2487fc99fba0f
heangdeveloper <simkimheang4@gmail.com>
2025-12-17 16:45:55 +0700

    update file

--- ui/src/pages/Home.tsx
+++ ui/src/pages/Home.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-import { useNavigate } from 'react-router';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay } from "swiper/modules";
 import "swiper/css";
@@ -8,38 +6,13 @@ import "swiper/css/navigation";
 import Slide01 from "../assets/img/slide/slide_bg_01.jpg"
 import Slide02 from "../assets/img/slide/slide_bg_02.jpg"
 
-interface Coin {
-    id: number;
-    name: string;
-    symbol: string;
-    price_usd: number;
-    percent_change_24h: number;
-}
-
 const Home = () => {
-    const [coins, setCoins] = React.useState<Coin[]>([]);
-    const navigate = useNavigate();
 
     const images = [
         { id: 1, src: Slide01, alt: "" },
         { id: 2, src: Slide02, alt: "" },
         { id: 3, src: Slide01, alt: "" },
     ];
-    
-    const fetchCoins = async () => {
-        const res = await fetch(`https://api.coinlore.net/api/tickers/?start=0&limit=10`);
-        const data = await res.json()
-        setCoins(data.data)
-        //console.log(data)
-    }
-        
-    React.useEffect(() => {
-        fetchCoins()
-        const interval = setInterval(() => {
-            fetchCoins();
-        }, 1000);
-        return () => clearInterval(interval);
-    }, [])
     return (
         <>
             <div className="slider__container">
