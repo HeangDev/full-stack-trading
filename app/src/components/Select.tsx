0e96ea397a472182082fd6b58cc63121464b6c35
heangdeveloper <simkimheang4@gmail.com>
2026-01-09 16:34:12 +0700

    update file

--- app/src/components/Select.tsx
+++ app/src/components/Select.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import TextField from './TextField';
+
+import { Icon } from '@iconify/react';
 
 interface OptionProps {
     index?: number;
     isActive?: boolean;
-    children: string;
-    onClick?: (index: number, text: string) => void;
+    value?: string;
+    label?: React.ReactNode;
+    children: React.ReactNode;
+    onClick?: (index: number, value?: string, label?: React.ReactNode) => void;
+}
+
+interface SelectProps {
+    placeholder?: string;
+    value?: string | number | readonly string[] | undefined;
+    children: React.ReactNode;
+    style?: React.CSSProperties
 }
 
 function cx(...parts: Array<string | false | undefined>) {
@@ -13,18 +25,114 @@ function cx(...parts: Array<string | false | undefined>) {
 
 export const Option: React.FC<OptionProps> = (props) => {
     return (
-        <div className="select__item">
-
-        </div>
+        <li
+            className={cx(props.isActive && "on")}
+            onClick={() =>
+                props.onClick?.(
+                    props.index || 0,
+                    props.value,
+                    props.label
+                )
+            }
+        >
+            {props.children}
+        </li>
     )
 }
 
-const Select = () => {
+export const Select: React.FC<SelectProps> = (props) => {
+    const [search, setSearch] = React.useState('');
+    const [activeIndex, setActiveIndex] = React.useState(0)
+    const [selectedValue, setSelectedValue] = React.useState<string | undefined>();
+    const [selectedLabel, setSelectedLabel] = React.useState<React.ReactNode>();
+    const [openSelect, setOpenSelect] = React.useState(false);
+    const dropdownRef = React.useRef<HTMLDivElement>(null);
+
+    const option = React.Children.toArray(props.children).filter(
+        (child) => React.isValidElement<OptionProps>(child)
+    )
+
+    const handleOptionClick = (index: number, value: string, label?: React.ReactNode) => {
+        setActiveIndex(index);
+        setSelectedValue(value);
+        setSelectedLabel(label);
+        setOpenSelect(false);
+    }
+
+    const handleToggleCombo = () => {
+        setOpenSelect((prev) => !prev);
+    }
+
+    const filteredOptions = option.filter((child) => {
+        if (!React.isValidElement<OptionProps>(child)) return false;
+
+        const text =
+            typeof child.props.children === 'string'
+                ? child.props.children
+                : String(child.props.children || "")
+        return text.toLowerCase().includes(search.toLowerCase())
+    });
+
+    React.useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+                setOpenSelect(false);
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        }
+    }, []);
+
     return (
         <>
-            
+            <div className="form__select" style={props.style} ref={dropdownRef}>
+                <div className="select__btn" onClick={handleToggleCombo} onChange={handleToggleCombo}>
+                    <div className="select__selected">
+                        {selectedLabel}
+                    </div>
+                    <div className="select__toggler">
+                        <Icon icon={
+                            openSelect
+                                ? "solar:alt-arrow-up-linear"
+                                : "solar:alt-arrow-down-linear"
+                        } />
+                    </div>
+                </div>
+                {openSelect && (
+                    <>
+                        <div className="select__menu">
+                            <div className="select__search">
+                                <TextField type="text" fullWidth placeholder="Search"
+                                    value={search}
+                                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => 
+                                        setSearch(e.target.value)
+                                    }
+                                />
+                            </div>
+                            <div className="select__list">
+                                {filteredOptions.length === 0 ? (
+                                    <div className="select__no__data">No results found</div>
+                                ) : (
+                                    <ul>
+                                        {filteredOptions.map((child, index) =>
+                                            React.isValidElement<OptionProps>(child)
+                                                ? React.cloneElement(child, {
+                                                    index,
+                                                    isActive: index === activeIndex,
+                                                    onClick: handleOptionClick,
+                                                } as OptionProps)
+                                                : child
+                                        )}
+                                    </ul>
+                                )}
+                            </div>
+                        </div>
+                    </>
+                )}
+            </div>
         </>
     )
 }
-
-export default Select
--- app/src/pages/account/Bank.tsx
+++ app/src/pages/account/Bank.tsx
@@ -1,11 +1,13 @@
 import TextField from '../../components/TextField';
 import Button from '../../components/Button'
+import { Select, Option } from '../../components/Select';
 
 import { useTranslation } from 'react-i18next'
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { bankSchema } from '../../schemas/bankSchema';
 import type { BankFormData } from '../../schemas/bankSchema';
+import { Icon } from '@iconify/react';
 
 const Bank = () => {
     const { t } = useTranslation();
@@ -21,6 +23,59 @@ const Bank = () => {
             <div className="auth__form">
                 <form onSubmit={handleSubmit(handleCreateBank)} autoComplete="off">
                     <div className="auth__form__container">
+                        <Select>
+                            <Option
+                                value="+855"
+                                label={
+                                    <div className="country__selected">
+                                        <Icon icon="circle-flags:kh" />
+                                        <span>+855</span>
+                                    </div>
+                                }
+                            >
+                                <div className="country__phone__container">
+                                    <div className="country__phone__info">
+                                        <Icon icon="circle-flags:kh" />
+                                        <div className="country__name">Cambodia</div>
+                                    </div>
+                                    <div className="country__code">+855</div>
+                                </div>
+                            </Option>
+                            <Option
+                                value="+86"
+                                label={
+                                    <div className="country__selected">
+                                        <Icon icon="circle-flags:cn" />
+                                        <span>+86</span>
+                                    </div>
+                                }
+                            >
+                                <div className="country__phone__container">
+                                    <div className="country__phone__info">
+                                        <Icon icon="circle-flags:cn" />
+                                        <div className="country__name">China</div>
+                                    </div>
+                                    <div className="country__code">+86</div>
+                                </div>
+                            </Option>
+                            <Option
+                                value="+66"
+                                label={
+                                    <div className="country__selected">
+                                        <Icon icon="circle-flags:th" />
+                                        <span>+66</span>
+                                    </div>
+                                }
+                            >
+                                <div className="country__phone__container">
+                                    <div className="country__phone__info">
+                                        <Icon icon="circle-flags:th" />
+                                        <div className="country__name">Thailand</div>
+                                    </div>
+                                    <div className="country__code">+66</div>
+                                </div>
+                            </Option>
+                        </Select>
                         <TextField type="text" fullWidth label={t('bank.bank_name')}
                             error={!!errors.bank_name}
                             helperText={errors.bank_name?.message}
